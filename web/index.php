ca6052af0adfe26674c14f4d242c18badace353c
falloon <falloon@college.harvard.edu>
2015-04-12 23:47:59 -0400

    updated heroku files

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Our web handlers
+
+include_once("opportunities.html");
+
+$app->run();
+
+?>
