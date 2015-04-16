042a518215624f0a8c8be983b280c60f04fefe47
falloon <falloon@college.harvard.edu>
2015-04-16 16:14:02 -0400

    Refactored all code, added inviting, backend initialization, routes, twig templating

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-DO GOOD TOGETHER
+# php-getting-started
+
+A barebones PHP bootstrap app that makes use of:
+
+* The [Silex](http://silex.sensiolabs.org/) web framework
+* [Twig](http://twig.sensiolabs.org/) templating
+* PDO database access
+
+## Running Locally
+
+Make sure you have PHP, Apache and Composer installed.  Also, install the [Heroku Toolbelt](https://toolbelt.heroku.com/).
+
+```sh
+git clone git@github.com:jonmountjoy/php-getting-started.git # or clone your own fork
+cd php-getting-started
+composer update
+foreman start web
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+```
+heroku create
+heroku addons:add heroku-postgresql:dev
+git push heroku master
+heroku open
+```
+
+You may want to add some data to the database too:
+
+```
+heroku pg:psql
+> CREATE TABLE test_table (id integer, name text);
+> INSERT INTO test_table VALUES (1, 'hello world');
+```
+
+## Documentation
+
+For more information about using PHP on Heroku, see these Dev Center articles:
+
+- [PHP on Heroku](https://devcenter.heroku.com/categories/php)
--- web/index.php
+++ web/index.php
@@ -10,9 +10,87 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Our web handlers
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
 
-include_once("opportunities.html");
+// Register the Postgres database add-on
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
+
+//////////////////////////////////////////////////////////
+// ROUTES
+//////////////////////////////////////////////////////////
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('homepage.html', array());
+});
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
+$app->get('/twig/{name}', function($name) use($app) {
+  return $app['twig']->render('index.twig', array(
+    'name' => $name,
+  ));
+});
+
+$app->get('/opportunities/', function() use($app) {
+  return $app['twig']->render('opportunities.html', array());
+});
+
+$app->get('/connect/', function() use($app) {
+  return $app['twig']->render('connect.html', array());
+});
+
+$app->get('/profile/', function() use($app) {
+  return $app['twig']->render('profile.html', array());
+});
+
+$app->get('/invite/', function() use($app) {
+  return $app['twig']->render('invite.html', array());
+});
+
+$app->get('/sent/{name}', function($name) use($app) {
+  return $app['twig']->render('sent.html', array(
+      'name' => $name,
+    ));
+});
+
+$app->get('/calendar/', function() use($app) {
+  return $app['twig']->render('calendar.html', array());
+});
+
+$app->get('/invitations/', function() use($app) {
+  return $app['twig']->render('invitations.html', array());
+});
+
+$app->get('/matches/', function() use($app) {
+  return $app['twig']->render('matches.html', array());
+});
+//////////////////////////////////////////////////////////
+// END OF ROUTES
+//////////////////////////////////////////////////////////
 
 $app->run();
 
